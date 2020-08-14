abf0e8e1df44e160f52e3df8c70346a13623c263
Erwin Yulianto <erw13n@gmail.com>
2020-08-15 05:20:21 +0700

    update package to latest version

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,7 @@ module.exports = {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
-            loader: 'file-loader',
-            options: {}  
+            loader: 'file-loader'
           }
         ]
       }
